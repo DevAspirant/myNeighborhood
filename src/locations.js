42fca920b3e1057b048d6eaad5245c6fce86e635
ammar falmban <devAspirant@gmail.com>
2018-07-15 10:33:27 +0300

    Add location.js file
    that file is for adding the location title, location and information

---
+++ src/locations.js
@@ -0,0 +1,32 @@
+/*
+Locations
+array that contain all the locations information
+*/
+export const  Locations = [
+  {
+    title: 'Red Sea Mall',
+    coordinates: {lat: 21.6278507, lng: 39.1111922},
+    infoWindowData:{}
+  },
+  {
+    title: 'Jarir Mall',
+    coordinates: {lat: 21.6400699, lng: 39.1315311},
+    infoWindowData:{}
+  },
+
+  {
+    title: 'Medd Cafe and Roastery',
+    coordinates: {lat: 21.644897, lng: 39.103389},
+    infoWindowData:{}
+  },
+  {
+    title: 'Haif & Kaif Cafe',
+    coordinates: {lat: 21.6427545, lng: 39.1029747},
+    infoWindowData:{}
+ },
+  {
+     title: 'Belajio Resort',
+     coordinates: {lat: 21.627351, lng: 39.105127},
+     infoWindowData:{}
+   }
+];
